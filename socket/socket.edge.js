31a674a81e23b7fdac80eea713fee77bb504dbfd
Brandon [White Fox] <r.rakeda@gmail.com>
2024-02-29 19:51:27 +0000

    data changes

--- socket/socket.edge.js
+++ socket/socket.edge.js
@@ -1,27 +1,44 @@
-const modelsBuilder = require('../models/models.builder');
+const modelsBuilder = require("../models/models.builder");
+const modelsEdges = require("../models/models.edges");
+const ObjectId = require('mongoose').Types.ObjectId;
 
 function update_edge(params, builder, socket) {
-    let edges = params.edges
-    let newEdges = params.newEdges
-    params.newEdges.map((edge) => {
-        if (edge.type == "remove") {
-            edges = edges.filter(item => item.id !== edge.id);
-        }
-    })
-
-    modelsBuilder.findByIdAndUpdate(builder, { "edges": edges }).then((e) => { })
-    socket.broadcast.to(builder).emit('edgeUpdate', newEdges);
+  let edges = params.edges;
+  let newEdges = params.newEdges;
+  params.newEdges.map((changedEdge) => {
+    if (changedEdge.type == "remove") {
+      const matchingEdge = edges.find((edge) => edge.id === changedEdge.id);
+      modelsBuilder.findByIdAndUpdate(
+        builder,
+        { $pull: { edges: new ObjectId(matchingEdge._id) } } // Correctly use $push to add to the array
+      ).then((e)=>{console.log("e2",e)}).catch((e3)=>{console.log("3e",e3)})
+    }
+  });
+  socket.broadcast.to(builder).emit("edgeUpdate", newEdges);
 }
 
-function connect_edge(params, builder, socket) {
-    let edges = params.edges
+async function connect_edge(params, builder, socket) {
+  let edges = params.edges;
 
-    edges.push(params.edge)
-    modelsBuilder.findByIdAndUpdate(builder, { "edges": edges }).then((e) => { })
-    socket.broadcast.to(builder).emit('onConnect', params.edge);
+  console.log(params)
+  // Assuming params.edge is the new edge object to be added
+  edges.push(params.edge);
+
+  // Create a new edge document and save it
+  const data = new modelsEdges(params.edge);
+  const dataToSave = await data.save();
+
+  // Use $push to add the new edge's ID to the 'edges' array of the specified builder document
+  await modelsBuilder.findByIdAndUpdate(
+    builder,
+    { $push: { edges: dataToSave._id } } // Correctly use $push to add to the array
+  );
+
+  // Broadcast the new edge to the specified room
+  socket.broadcast.to(builder).emit("onConnect", params.edge);
 }
 
 module.exports = {
-    update_edge,
-    connect_edge
-}
+  update_edge,
+  connect_edge,
+};
--- socket/socket.node.js
+++ socket/socket.node.js
@@ -1,77 +1,51 @@
-const nodesBuilder = require('../models/models.nodes');
-const modelsBuilder = require('../models/models.builder');
-
+const nodesBuilder = require("../models/models.nodes");
+const modelsBuilder = require("../models/models.builder");
 
 async function create_node(node, builder, socket) {
-    let newNode = node.newNode
-
-    /*let fields = {}
-    switch (node.newNode.type) {
-        case "functionNode": fields["out"] = { [node.newNode.data.function]: { "type": node.newNode.data.function, "readOnly": true, "example": 1 } }
-    }
-
-    let newNode = node.newNode
-    newNode.data["node_data"] = fields
-
-    console.log("create node", newNode.data)
-
-    const savedData = await new nodesBuilder(newNode.data).save()
-    newNode["node_id"] = savedData._id;
-
-    console.log("savedData", savedData)
-
-
-    console.log("builker", newNode)
-
-
-    socket.broadcast.to(builder).emit('nodeCreate', { newNode: newNode });*/
-
-    delete newNode.data
-
-    newNode["node_id"] = newNode.id;
-
-    console.log("newNode",newNode)
-    modelsBuilder.findByIdAndUpdate(builder, { $push: { "nodes": newNode } }, { new: true });
 
 }
 
-
 function delete_node(nodes, builder, socket) {
-    nodes.map((node) => {
-        modelsBuilder.findByIdAndUpdate(builder, { $pull: { nodes: { id: node.id } } }).then((e) => { console.log(e) })
-        nodesBuilder.findByIdAndDelete(node.node_id)
-    })
-    socket.broadcast.to(builder).emit('nodeDelete', nodes);
+  nodes.map((node) => {
+    modelsBuilder
+      .findByIdAndUpdate(builder, { $pull: { nodes: node.id } })
+      .then((e) => {
+        console.log(e);
+      });
+    nodesBuilder.findByIdAndDelete(node.node_id);
+  });
+  socket.broadcast.to(builder).emit("nodeDelete", nodes);
 }
 
 function update_node(node, builder, socket) {
-    node.nodes.map((nod) => {
-        delete nod.data
-    })
-    console.log("node updated")
-    console.log(node.nodes)
-    modelsBuilder.findByIdAndUpdate(builder, { "nodes": node.nodes }).then((e) => { })
-    socket.broadcast.to(builder).emit('nodeUpdate', { newNodes: node.newNodes });
+  node.changedNodes.map((nod) => {
+    nodesBuilder.findByIdAndUpdate(nod._id, nod).exec();
+  });
+  socket.broadcast
+    .to(builder)
+    .emit("nodeUpdate", { changedNodes: node.changedNodes });
 }
 
 function update_node_data(params, builder, socket) {
+  const id = params.id.split("-")[1];
+  console.log("updated node data", params.value);
 
-    const id = params.id.split("-")[1]
-    console.log("updated node data", params.value)
+  if (id) {
+    console.log("id", id);
+    console.log("params", params);
+    nodesBuilder
+      .findByIdAndUpdate(id, { inputData: params.value })
+      .then((e) => {
+        console.log("e", e);
+      });
 
-    if (id) {
-        console.log("id", id)
-        console.log("params", params)
-        nodesBuilder.findByIdAndUpdate(id, { inputData: params.value }).then((e) => { console.log("e", e) })
-
-        socket.broadcast.to(builder).emit('updateField', params);
-    }
-
+    socket.broadcast.to(builder).emit("updateField", params);
+  }
 }
 
 module.exports = {
-    create_node,
-    delete_node,
-    update_node,
-    update_node_data
-}
+  create_node,
+  delete_node,
+  update_node,
+  update_node_data,
+};
